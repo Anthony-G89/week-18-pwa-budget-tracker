660092002331f7a076a00ef9578f0211dae56a4b
Anthony Guerrero <adguerrero1935@gmail.com>
2020-05-07 19:25:06 -0400

    fixed server

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+app.use(compression());
+
 
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
