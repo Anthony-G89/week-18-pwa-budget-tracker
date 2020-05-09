98855501bc99227f295cce1ba82209379783cdd4
Anthony Guerrero <adguerrero1935@gmail.com>
2020-05-09 09:15:24 -0400

    Fixing server. Github not working

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 app.use(compression());
+
+
 
 
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
